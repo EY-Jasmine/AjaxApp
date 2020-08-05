ad6cd5ee8b6011336014e6c2d1e682fcb60dc8e8
EY-Jasmine <s81.yoshida.emi@gmail.com>
2020-08-05 16:22:35 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,8 +20,4 @@ class PostsController < ApplicationController
     item = Post.find(params[:id])
     render json: { post: item }
   end
-<<<<<<< Updated upstream
-=======
-  
->>>>>>> Stashed changes
 end
